29a60f977e995cd65fb20f4e28c40f5146aa7d9a
Maen22 <m3n.ibregeth@gmail.com>
2021-07-16 20:38:24 +0300

    added main section component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from "./components/header/header";
 import Hero from "./components/hero/hero";
+import MainSection from "./components/main-section/main-section";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <MainSection />
     </div>
   );
 }
--- src/components/main-section/main-section.js
+++ src/components/main-section/main-section.js
@@ -1,18 +1,35 @@
-import "main-section.styles.scss";
+import "./main-section.styles.scss";
 import { withRouter } from "react-router-dom";
 import studioBag from "../../assests/studio-bag.png";
 
 const MainSection = ({ history }) => {
+  const onClickBtn = () => history.push("/product/1");
+
   return (
     <div className="main-section-container">
       <div className="main-section-middle">
         <div className="ms-m-image">
           <img src={studioBag} alt="studio bag" />
         </div>
-        <div className="ms-m-description"></div>
+        <div className="ms-m-description">
+          <h2>Designed for fashion, Crafted for sport.</h2>
+          <p>
+            We make products that effortlessly transition from day to night.
+            From the board room to the fitness studio, and everywhere in
+            between, each Nomads piece is thoughtfully created to be the perfect
+            balance of form and function.
+          </p>
+          <button
+            className="button is-black"
+            id="shop-now"
+            onClick={onClickBtn}
+          >
+            STUDIO BAG
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
-export default MainSection;
+export default withRouter(MainSection);
